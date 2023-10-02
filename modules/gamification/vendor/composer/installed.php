de1b3da7da21d1a91d1d28a790a41fec7e398ed0
Yuler Figueroa <yuleralex1@gmail.com>
2023-10-01 23:10:48 -0500

    add prestashop

---
+++ modules/gamification/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/gamification',
+        'pretty_version' => 'v3.0.2',
+        'version' => '3.0.2.0',
+        'reference' => 'a9a0160b3b31df84ddd6cb2f023082f82fe8326b',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/gamification' => array(
+            'pretty_version' => 'v3.0.2',
+            'version' => '3.0.2.0',
+            'reference' => 'a9a0160b3b31df84ddd6cb2f023082f82fe8326b',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
